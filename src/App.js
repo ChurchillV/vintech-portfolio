a14546b6038b2e5c8cc4dc6927571dd31a13d149
Vince Churchill <vincechurchillankrah@gmail.com>
2024-09-10 16:47:53 +0000

    Changed favicon

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Helmet>
         <title>Vince Churchill's Portfolio</title>
         <meta name="description" content="Welcome to Vince Churchill's portfolio! 🌟 I'm a passionate software developer from Ghana with expertise in building full-stack web apps. Explore my journey as I blend my love for databases, backend APIs, and frontend design to craft innovative solutions. Dive into my CV, discover my projects, and see how I tackle everyday problems with technology. Let's connect!" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/images/VINTECH..jpg" type="image/x-icon" />
       </Helmet>
       <Navbar />
       <Hero />
