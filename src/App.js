98b5a81fd3352907b643764da2bada5a61e2dcb2
Vince Churchill <vincechurchillankrah@gmail.com>
2024-09-11 16:44:24 +0000

    Cloudinary favicon.ico

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Helmet>
         <title>Vince Churchill | Portfolio</title>
         <meta name="description" content="Welcome to Vince Churchill's portfolio! 🌟 I'm a passionate software developer from Ghana with expertise in building full-stack web apps. Explore my journey as I blend my love for databases, backend APIs, and frontend design to craft innovative solutions. Dive into my CV, discover my projects, and see how I tackle everyday problems with technology. Let's connect!" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="https://res.cloudinary.com/dpyvx3nd9/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1726072985/apple-touch-icon_oqxach.png" type="image/x-icon" />
       </Helmet>
       <Navbar />
       <Hero />
